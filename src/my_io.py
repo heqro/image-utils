d165bb0dc5a9048503c62a4cc40155d5019d6094
Rodrigo Iglesias <hr.iglesias.2018@alumnos.urjc.es>
2025-02-11 09:40:39 +0000

    🖥️ Rename and add print image function

--- src/my_io.py
+++ src/my_io.py
@@ -16,6 +16,7 @@ def __load_image__(path: str, mode: torchvision.io.ImageReadMode) -> Tensor:
     """
     return torchvision.io.read_image(path, mode) / 255
 
+
 def load_rgb_image(path: str) -> Tensor:
     """
     Load an RGB image from a file path.
@@ -27,7 +28,6 @@ def load_rgb_image(path: str) -> Tensor:
         torch.Tensor: Normalized RGB image tensor with values in [0,1]
     """
     return __load_image__(path, torchvision.io.image.ImageReadMode.RGB)
-
 
 
 def load_gray_image(path: str, as_mask=False) -> Tensor:
@@ -65,3 +65,18 @@ def load_serialized_image(path: str, is_mask=False, normalize=True) -> Tensor:
     if normalize:
         img = (img - img.min()) / (img.max() - img.min())
     return img
+
+
+def print_image(img: ndarray, file_name: str | None):
+    if file_name is None:
+        import matplotlib.pyplot as plt
+
+        plt.figure(figsize=(img.shape[1] / 100, img.shape[0] / 100), dpi=100)
+        plt.imshow(img)
+        plt.axis("off")
+        plt.show()
+        plt.close()
+    else:
+        from imageio import imwrite
+
+        imwrite(uri=file_name, im=img)
